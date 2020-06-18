347f17b6a29a82afdb30d6ef8180a0149bede1b6
起点 <widespread@sina.com>
2020-06-18 13:41:07 +0800

    code AST

--- sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/ast/cond/ConditionGroup.java
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/ast/cond/ConditionGroup.java
@@ -1,18 +1,17 @@
 package com.dangdang.ddframe.rdb.sharding.parsing.parser.ast.cond;
 
+import com.dangdang.ddframe.rdb.sharding.parsing.parser.ast.rel.EConditionGroup;
 import com.dangdang.ddframe.rdb.sharding.parsing.parser.ast.rel.ELogicType;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import java.util.LinkedList;
-import java.util.List;
-
 @RequiredArgsConstructor
 @Setter
 @Getter
 public class ConditionGroup {
+    private final EConditionGroup conditionGroup;
+    private final ConditionItem item;
     private final ELogicType logicType;
-    private final ConditionItem left;
-    private final ConditionGroup right;
+    private final ConditionGroup next;
 }
--- sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/ast/rel/EConditionGroup.java
+++ sharding-jdbc-core/src/main/java/com/dangdang/ddframe/rdb/sharding/parsing/parser/ast/rel/EConditionGroup.java
@@ -1,8 +1,8 @@
 package com.dangdang.ddframe.rdb.sharding.parsing.parser.ast.rel;
 
-public enum ELogicType {
-    AND,
-    OR,
+public enum EConditionGroup {
+    START,
+    END,
 
     NONE
 }
